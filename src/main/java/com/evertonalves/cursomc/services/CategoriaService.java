dc7cbcc4802e84eeb524e27004c743a7e9110410
Everton Alves <everton10alves@gmail.com>
2019-01-13 16:17:01 -0300

    Buscando categorias com DTO

--- src/main/java/com/evertonalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/evertonalves/cursomc/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package com.evertonalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.evertonalves.cursomc.domain.Categoria;
 import com.evertonalves.cursomc.repositories.CategoriaRepository;
+import com.evertonalves.cursomc.services.exceptions.DataIntegrityException;
 import com.evertonalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -24,5 +27,24 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj){
+		buscar(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
