1c9837951d1f9e1040524dfe4995ca188a759875
Everton Alves <everton10alves@gmail.com>
2019-11-03 16:24:44 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/evertonalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/evertonalves/cursomc/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +40,7 @@ public class CategoriaResource {
 	
 	
 	//INSERINDO UMA NOVA CATEGORIA
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
 		Categoria obj =  service.fromDTO(objDTO);
@@ -51,6 +53,7 @@ public class CategoriaResource {
 	}
 	
 	//ATUALIZANDO UMA CATEGORIA
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDTO, @PathVariable Integer id){
 		Categoria obj =  service.fromDTO(objDTO);
@@ -62,6 +65,7 @@ public class CategoriaResource {
 	}
 	
 	//DELETANDO UMA NOVA CATEGORIA
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 			
--- src/main/java/com/evertonalves/cursomc/resources/ClienteResource.java
+++ src/main/java/com/evertonalves/cursomc/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,6 +62,7 @@ public class ClienteResource {
 		}
 		
 		//DELETANDO UMA NOVA CATEGORIA
+		@PreAuthorize("hasAnyRole('ADMIN')")
 		@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 		public ResponseEntity<Void> delete(@PathVariable Integer id) {
 				
@@ -69,6 +71,7 @@ public class ClienteResource {
 		}
 		
 		//BUSCANDO TODOS CLIENTES
+		@PreAuthorize("hasAnyRole('ADMIN')")
 		@RequestMapping(method=RequestMethod.GET)
 		public ResponseEntity<List<ClienteDTO>> findAll() {
 				
